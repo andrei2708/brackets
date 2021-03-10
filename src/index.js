830ded3bb1d6bffc14183e7908f499136a490e57
Andrei Pauliukovich <apavlukovich@gmail.com>
2021-03-10 19:46:40 +0300

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const pairsBrackets = bracketsConfig.map(pair => pair.join(''))
+    for (let i = 0; i < pairsBrackets.length;) {
+      if (str.includes(pairsBrackets[i])) {
+        str = str.replace(pairsBrackets[i], '')
+        i = 0
+      }
+      else i++
+    }
+    return str === ''
 }
